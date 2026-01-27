e80a00c4e8768754b40229ac3d72d67befc8fa42
henrialbano <henrialbano@hotmail.com>
2026-01-27 19:32:12 -0300

    feat: initial web app and infra structure

---
+++ infra/terraform/dev/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "vercel" {
+  # O token virá de variável de ambiente:
+  # VERCEL_API_TOKEN
+}
